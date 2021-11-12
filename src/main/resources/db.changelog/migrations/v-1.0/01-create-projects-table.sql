394c4b4145c058dcd565de9e645c7e4d29fae714
Marat Ayupov <marat_0988@inbox.ru>
2021-11-13 00:51:54 +0300

    2 commit finish change sets and ammend some previous files

--- src/main/resources/db.changelog/migrations/v-1.0/01-create-projects-table.sql
+++ src/main/resources/db.changelog/migrations/v-1.0/01-create-projects-table.sql
@@ -1,4 +1,3 @@
-
 create table projects (
 projectId Long not nul auto_increment,
 name varchar(255) not null,
@@ -7,5 +6,15 @@ primary key (project ID)
 ) engine=InnoDB
 GO
 alter table projects
-add constraint UK_r43af9ap4edm43mmt01odd16 unique (User name)\
+add constraint UK_r43af9ap4edm43mmt01odd16 unique (name)
+GO
+alter table projects
+   add constraint FKtpkopyby8qwu1noj4n4t3yuig
+   foreign key (userId)
+   references users (userId)
+GO
+alter table projects
+   add constraint FKtpkopyby8qwu1noj4n4t3yuig
+   foreign key (statusId)
+   references statuses (statusId)
 GO
--- src/main/resources/db.changelog/migrations/v-1.0/01-drop-projects-table.sql
+++ src/main/resources/db.changelog/migrations/v-1.0/01-drop-projects-table.sql
@@ -1,6 +1,11 @@
 
 alter table projects
 drop index UK_r43af9ap4edm43mmt01odd16
+alter table projects
+    drop constraint FKtpkopyby8qwu1noj4n4t3yuig
+GO
+alter table projects
+    drop constraint FKtpkopyby8qwu1noj4n4t3yuig
 GO
 drop table  projects
 GO
